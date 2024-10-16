4bd82748ea829af8340d39c81f1e4f932e81bf34
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-10-16 12:12:07 +0900

    #7 setup admin tool

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):  # change layout from Stacked to Tabular
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["id", "question_text", "pub_date", "was_published_recently"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 
@@ -7,6 +8,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def __str__(self):
         return self.question_text
 
