affd8b9c1ca30b7a9f6d9d187d9fb4b89b0e63f9
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-10-24 11:44:31 +0900

    #9 feature show publish state

--- event/models.py
+++ event/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
