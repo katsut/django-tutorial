91e793d22cd62239cd5150724aefd2c4261bbaef
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-10-24 23:54:24 +0900

    #10 feature add choices

--- event/models.py
+++ event/models.py
@@ -10,3 +10,11 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Choice(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="choices")
+    date = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.date
--- event/views.py
+++ event/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from event.models import Event
+from event.models import Choice, Event
 from logging import getLogger
 
 logger = getLogger(__name__)
@@ -14,7 +14,12 @@ def index(request):
 
 def create(request):
     name = request.POST["name"]
-    event = Event(name=name, description="test").save()
+    datelist = request.POST["datelist"]
+    event = Event(name=name, description="test")
+    event.save()
+    choices = Choice.objects.bulk_create(
+        [Choice(event=event, date=date) for date in datelist.split("\n")]
+    )
     logger.info(event)
     return HttpResponseRedirect(reverse("event:index"))
 
@@ -25,11 +30,25 @@ def edit(request, event_id):
 
 
 def update(request, event_id):
-    # TODO: implements
-    logger.info("update")
     event = Event.objects.get(id=event_id)
     event.name = request.POST["name"]
     event.description = request.POST["description"]
     event.published = request.POST["published"]
     event.save()
+
+    choices = event.choices.all()
+    for choice in choices:
+        date = request.POST.get(f"choice-{choice.id}")
+        if choice:
+            choice.date = date
+            choice.save()
+        else:
+            choice.delete()
+    new_choices = request.POST.get("new_choices")
+    if new_choices:
+        new_choices = new_choices.split("\n")
+        choices = Choice.objects.bulk_create(
+            [Choice(event=event, date=date) for date in new_choices]
+        )
+
     return HttpResponseRedirect(reverse("event:index"))
