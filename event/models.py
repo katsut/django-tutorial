7481400e7e829aa7fe6bec15086eadae9037b67b
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-10-30 10:51:20 +0900

    #11 fix model

--- event/models.py
+++ event/models.py
@@ -23,7 +23,7 @@ class Choice(models.Model):
 class Vote(models.Model):
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name="votes")
     name = models.CharField(max_length=200)
-    state = models.IntegerField(required=True)  # 0: ✕ , 1: △, 2: ◯
+    state = models.IntegerField(null=False)  # 0: ✕ , 1: △, 2: ◯
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
